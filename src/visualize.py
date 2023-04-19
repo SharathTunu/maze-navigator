51e344866d3ceb01f556a6e6ffbf135b9defe549
SharathTunu <41936662+SharathTunu@users.noreply.github.com>
2023-04-19 09:10:27 -0700

    Update visualize script to fill in the maze with details

--- src/visualize.py
+++ src/visualize.py
@@ -9,10 +9,41 @@ class visualize:
         self.images = []
     
     def __drawOutlines(self, draw: ImageDraw) -> ImageDraw:
-        pass
+        """
+        1. Fill the walls with black and leave the paths white
+        2. Add gray dots to the open path.
+        """
+        for i in range(len(self.maze)):
+            for j in range(len(self.maze[i])):
+                color = (255, 255, 255)
+                r = 0
+                # 1.
+                if self.maze[i][j] == 1:
+                    color = (0, 0, 0)
+                if [i,j] in [self.entrance, self.exit]:
+                    color = (0, 255, 0)
+                    r = self.borders
+                
+                draw.rectangle((j*self.pxls+r, i*self.pxls+r,
+                                j*self.pxls+self.pxls-r-1, i*self.pxls+self.pxls-r-1), fill=color)
+                # 2.
+                if self.maze[i][j] == 2:
+                    r = self.borders
+                    draw.ellipse((j * self.pxls + r, i * self.pxls + r,
+                                  j * self.pxls + self.pxls - r - 1, i * self.pxls + self.pxls - r - 1),
+                                 fill=(128, 128, 128))
+        return draw
 
     def __drawPaths(self, thePath: list, draw: ImageDraw) -> ImageDraw:
-        pass
+        """Connect the dots drawn above with lines that show the path explored by dfs
+        """
+        for u in range(len(thePath)-1):
+            y = thePath[u][0]*self.pxls + int(self.pxls/2)
+            x = thePath[u][1]*self.pxls + int(self.pxls/2)
+            y1 = thePath[u+1][0]*self.pxls + int(self.pxls/2)
+            x1 = thePath[u+1][1]*self.pxls + int(self.pxls/2)
+            draw.line((x, y, x1, y1), fill=(255, 0, 0), width=5)
+        return draw
 
     def drawMaze(self, thePath:list = []) -> None:
         """
